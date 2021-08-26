71e7cc437ba8a9e1b4808ca58a14df521eb6ef41
Patrik Buhring <patrikbuhring@gmail.com>
2021-08-26 02:47:39 -0400

    Add `ValidateResult` api.

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,18 @@
-use std::sync::atomic::{AtomicUsize, Ordering, AtomicBool};
 use parking_lot_core::{park, unpark_all, DEFAULT_PARK_TOKEN, DEFAULT_UNPARK_TOKEN};
+use std::sync::atomic::{AtomicBool, Ordering};
+
+pub enum ValidateResult {
+    Success,
+    Abort { run_hook: bool },
+    Retry,
+}
 
 pub struct PatientWaiter(AtomicBool);
 
 impl PatientWaiter {
-    pub fn new() -> Self { PatientWaiter(AtomicBool::new(false)) }
+    pub fn new() -> Self {
+        PatientWaiter(AtomicBool::new(false))
+    }
 
     pub fn notify(&self) {
         if self.0.load(Ordering::Relaxed) {
@@ -14,19 +22,16 @@ impl PatientWaiter {
 
     #[cold]
     fn notify_slow(&self) {
-        let key = self as usize;
+        let key = self as *const _ as usize;
         unsafe {
-            unpark_all(
-                key,
-                DEFAULT_UNPARK_TOKEN,
-            );
+            unpark_all(key, DEFAULT_UNPARK_TOKEN);
         }
 
         self.0.store(false, Ordering::Relaxed);
     }
 
     pub fn wait(&self, hook: impl FnOnce()) {
-        let key = self as usize;
+        let key = self as *const _ as usize;
 
         let validate = || true;
         let timeout = |_, _| {};
@@ -39,19 +44,17 @@ impl PatientWaiter {
         };
 
         unsafe {
-            park(
-                key,
-                validate,
-                hook,
-                timeout,
-                DEFAULT_PARK_TOKEN,
-                None,
-            );
+            park(key, validate, hook, timeout, DEFAULT_PARK_TOKEN, None);
         }
     }
 
-    pub fn wait_until(&self, hook: impl FnOnce(), mut is_done: impl FnMut() -> bool, mut lock_and_validate: impl FnMut() -> bool) {
-        let key = self as usize;
+    pub fn wait_until(
+        &self,
+        mut hook: impl FnMut(),
+        mut is_done: impl FnMut() -> bool,
+        mut lock_and_validate: impl FnMut() -> ValidateResult,
+    ) {
+        let key = self as *const _ as usize;
 
         let validate = || true;
         let timeout = |_, _| {};
@@ -64,43 +67,28 @@ impl PatientWaiter {
         };
 
         unsafe {
-            park(
-                key,
-                validate,
-                hook,
-                timeout,
-                DEFAULT_PARK_TOKEN,
-                None,
-            );
+            park(key, validate, hook, timeout, DEFAULT_PARK_TOKEN, None);
         }
 
-        let hook = || {
+        let unlock = || {
             waiting.store(true, Ordering::Relaxed);
         };
 
         loop {
             if !is_done() {
                 unsafe {
-                    park(
-                        key,
-                        validate,
-                        hook,
-                        timeout,
-                        DEFAULT_PARK_TOKEN,
-                        None,
-                    );
+                    park(key, validate, unlock, timeout, DEFAULT_PARK_TOKEN, None);
                 }
-            } else if lock_and_validate() {
-                break;
+            } else {
+                match lock_and_validate {
+                    ValidateResult::Abort { run_hook: true } => {
+                        unlock();
+                        return;
+                    }
+                    ValidateResult::Abort { run_hook: false } | ValidateResult::Success => return,
+                    ValidateResult::Retry => unlock(),
+                }
             }
         }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
     }
 }
